6c88d02aa06858c5cddaf40b87f1be3db3cfc159
salah <salah@git.com>
2020-11-18 23:23:05 +0100

    Ajoutez des fichiers projet.

---
+++ TP_Temps/Temps.cpp
@@ -0,0 +1,21 @@
+#include "Temps.h"
+
+void Temps::getH() const
+{
+}
+
+void Temps::getM() const
+{
+}
+
+void Temps::getS() const
+{
+}
+
+Temps::Temps()
+{
+}
+
+Temps::Temps(int, int, int)
+{
+}
---
+++ TP_Temps/Temps.h
@@ -0,0 +1,12 @@
+#pragma once
+class Temps
+{
+private:
+	int heure;
+	int minute;
+	int seconde;
+public :
+	Temps();
+	Temps(int, int, int);
+};
+
